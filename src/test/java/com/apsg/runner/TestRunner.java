4aedf6188b48efb5704a9e376392ec10b594fc89
AJAYPGUJJAR <ajaypgujjar@yahoo.com>
2025-08-17 15:11:53 +0530

    fix: Using classpath: ensures it works both locally and in CI/CD environments.

--- src/test/java/com/apsg/runner/TestRunner.java
+++ src/test/java/com/apsg/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Sample.feature",
+        features = "classpath:features/Sample.feature",
         glue = "com.apsg.uitests.stepdefs",
         plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
 )
