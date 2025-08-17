e15549dbb8d5e964ec427056cb7b43987a5e206b
AJAYPGUJJAR <ajaypgujjar@yahoo.com>
2025-08-17 15:37:32 +0530

    feat: renamed test report generation folder name

--- src/test/java/com/apsg/runner/TestRunner.java
+++ src/test/java/com/apsg/runner/TestRunner.java
@@ -6,7 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "classpath:features/Sample.feature",
         glue = "com.apsg.uitests.stepdefs",
-        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-html-report.html",
+                "json:target/cucumber-reports/cucumber.json"
+        }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
